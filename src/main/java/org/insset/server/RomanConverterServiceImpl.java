610fc0017389abb03756661bba4b3355fd9bc8dc
morgan <morgan.jouard@gmail.com>
2019-11-07 15:45:49 +0100

    3e commit, convertisseur romain fonctionnel

--- src/main/java/org/insset/server/RomanConverterServiceImpl.java
+++ src/main/java/org/insset/server/RomanConverterServiceImpl.java
@@ -32,24 +32,16 @@ public class RomanConverterServiceImpl extends RemoteServiceServlet implements
     @Override
     public Integer convertRomanToArabe(String nbr) throws IllegalArgumentException {
         
-       
-        int i = 1;
-        int v = 5;
-        int x = 10;
-        int l = 50;
-        int c = 100;
-        int d = 500;
-        int m = 1000;
         int rom = 0;
         int posi = 0;
         int combo = 0;
         String charActual = "a";
         String charNext = "a";
-        String charPrev = "a";
         int valCharActual = 0;
         int valCharNext = 0;
-        int valCharPrev = 0;
-        //toLowerCase(nbr);
+        
+        
+        toLowerCase(nbr);
         
         int count =0;
         
@@ -58,112 +50,88 @@ public class RomanConverterServiceImpl extends RemoteServiceServlet implements
         
         while(count < nbr.length()){
             
-            
-            
             //on recupere les charactere dans un string (3 a chaque fois)
             charActual = String.valueOf(nbr.charAt(posi));
             if(posi < nbr.length()-1){
                 charNext = String.valueOf(nbr.charAt(posi+1));
             }
-            posi++;
-            charPrev = String.valueOf(nbr.charAt(posi-1));
-            posi = posi-1;
             
+            //le chiffre romain traduit en valeur
+            valCharActual = giveValChar(charActual);
+            //idem pour le caractere suivant
+            valCharNext = giveValChar(charNext);
             
-            //boucle qui verifie la regle des 3 chiffre d'affile
-            while((charActual.equals(charNext) || charPrev.equals(charActual)) && (combo < 3) && (posi < nbr.length()-1)){
+            //if qui verifie la regle des 3 chiffre d'affile
+            if((valCharActual >= valCharNext)  && (combo < 3) && (posi < nbr.length()-1)){
                 posi = posi+1;
-                combo++;
-                switch (charActual) {
-
-                    case "i":
-                        rom = rom +1;
-                        valCharActual = 1;
-                        valCharNext = 1;
-                    break;
-                    case "v":
-                        rom = rom +5;
-                        valCharActual = 5;
-                        valCharNext = 5;
-                    break;
-                    case "x":
-                        rom = rom +10;
-                        valCharActual = 10;
-                        valCharNext = 10;
-                    break;
-                    case "l":
-                        rom = rom +50;
-                        valCharActual = 50;
-                        valCharNext = 50;
-                    break;
-                    case "c":
-                        rom = rom +100;
-                        valCharActual = 100;
-                        valCharNext = 100;
-                    break;
-                    case "d":
-                        rom = rom +500;
-                        valCharActual = 500;
-                        valCharNext = 500;
-                    break;
-                    case "m":
-                        rom = rom +1000;
-                        valCharActual = 1000;
-                        valCharNext = 1000;
-                    break;
-                   
+                
+                if(valCharActual == valCharNext){
+                    combo++;
                 }
-
+                else{
+                    combo =0;
+                }
+                rom = rom + valCharActual;
+                
                 if(" ".equals(charNext)){
                     
                 } else {
                     charActual = String.valueOf(nbr.charAt(posi));
                     charNext = String.valueOf(nbr.charAt(posi+1));
-                    charPrev = String.valueOf(nbr.charAt(posi-1));  
                 }
+                
                 
 
             }
-            //reset du combo
-            combo = 0;
+            else if(valCharActual < valCharNext){
+                posi = posi+1;
+                rom = rom - valCharActual;
+                
+                if(" ".equals(charNext)){
+                    
+                } else {
+                    charActual = String.valueOf(nbr.charAt(posi));
+                    charNext = String.valueOf(nbr.charAt(posi+1));
+                }
+            }
             count++;
-            /*
-            //on donner les valeurs des chiffre actuel, precedent et suivant en int
-            switch (charActual) {
+        }
+       
+       return rom;
+    }
+    
+    
+      public static int giveValChar(String lettre){
+        //Fonction qui retourne la valeur de du charactere qu'on lui donne
+        int val =0;
+        switch (lettre) {
 
                     case "i":
-                        rom = rom +1;
-                        break;
+                        val = 1;
+                    break;
                     case "v":
-                        rom = rom +5;
-                        break;
+                        val = 5;
+                    break;
                     case "x":
-                        rom = rom +10;
-                        break;
+                        val = 10;
+                    break;
                     case "l":
-                        rom = rom +50;
-                        break;
+                        val = 50;
+                    break;
                     case "c":
-                        rom = rom +100;
-                        break;
+                        val= 100;
+                    break;
                     case "d":
-                        rom = rom +500;
-                        break;
+                        val = 500;
+                    break;
                     case "m":
-                        rom = rom +1000;
-                        break;
+                        val = 1000;
+                    break;
                 }
-            
-            if(charActual < charPrev && charActual < charNext){
-                
-                
-            }
-           
-           */
-        }
+        return val;
         
-       
-        return rom;
     }
+    
+      
 
 }
