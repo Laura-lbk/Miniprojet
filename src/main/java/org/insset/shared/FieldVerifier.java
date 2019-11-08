317b31a7bf2994f7edd57145b9bcad36309c69d5
morgan <morgan.jouard@gmail.com>
2019-11-08 11:07:36 +0100

    Projet tres fonctionnel (deux parties marchent)

--- src/main/java/org/insset/shared/FieldVerifier.java
+++ src/main/java/org/insset/shared/FieldVerifier.java
@@ -1,5 +1,8 @@
 package org.insset.shared;
 
+
+import static com.google.gwt.thirdparty.guava.common.base.Ascii.toLowerCase;
+
 /**
  * <p>
  * FieldVerifier validates that the name the user enters is valid.
@@ -58,13 +61,116 @@ public class FieldVerifier {
         return true;
     }
 
+    
     public static boolean isValidRoman(String nbr) {
         //Implement your code
+        
+        int posi = 0;
+        int combo = 0;
+        String charActual = "a";
+        String charNext = "a";
+        int valCharActual = 0;
+        int valCharNext = 0;
+        
+        
+        nbr.toLowerCase();
+        
+        int count =0;
+        
+        //neccessaire pour donner la sortie du while
+        nbr = nbr +" ";
+        
+        while(count < nbr.length()){
+            
+            //on recupere les charactere dans un string (3 a chaque fois)
+            charActual = String.valueOf(nbr.charAt(posi));
+            if(posi < nbr.length()-1){
+                charNext = String.valueOf(nbr.charAt(posi+1));
+            }
+            
+            //le chiffre romain traduit en valeur
+            valCharActual = giveValChar(charActual);
+            //idem pour le caractere suivant
+            valCharNext = giveValChar(charNext);
+            
+            //if qui verifie la regle des 3 chiffre d'affile
+            if((valCharActual >= valCharNext)  && (combo <= 4) && (posi < nbr.length()-1)){
+                posi = posi+1;
+                
+                if(valCharActual == valCharNext){
+                    combo++;
+                }
+                if(combo == 4){
+                    return false;
+                }
+                
+                if(" ".equals(charNext)){
+                    
+                } else {
+                    charActual = String.valueOf(nbr.charAt(posi));
+                    charNext = String.valueOf(nbr.charAt(posi+1));
+                }
+                
+                
+
+            }
+            else if(valCharActual < valCharNext){
+                posi = posi+1;
+                
+                if(" ".equals(charNext)){
+                    
+                } else {
+                    charActual = String.valueOf(nbr.charAt(posi));
+                    charNext = String.valueOf(nbr.charAt(posi+1));
+                }
+            }
+            count++;
+        }
+     
         return true;
     }
 
+    
     public static boolean isValidDate(String date) {
         //Implement your code
         return true;
     }
+
+
+
+
+    public static int giveValChar(String lettre){
+            //Fonction qui retourne la valeur de du charactere qu'on lui donne
+            int val =0;
+            switch (lettre) {
+
+                        case "i":
+                            val = 1;
+                        break;
+                        case "v":
+                            val = 5;
+                        break;
+                        case "x":
+                            val = 10;
+                        break;
+                        case "l":
+                            val = 50;
+                        break;
+                        case "c":
+                            val= 100;
+                        break;
+                        case "d":
+                            val = 500;
+                        break;
+                        case "m":
+                            val = 1000;
+                        break;
+                    }
+            return val;
+    }
+
+
+
+
+
 }
